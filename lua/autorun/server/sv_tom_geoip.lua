38ed0d5e04486423cc8e4ac36b12e545465a5125
Tom <tomtheawesomest@gmail.com>
2020-05-14 00:12:05 +0100

    Add addon description

--- lua/autorun/server/sv_tom_geoip.lua
+++ lua/autorun/server/sv_tom_geoip.lua
@@ -1,6 +1,6 @@
 
 --[[-------------------------------------------------------------------------
-Tom's GeoIP Blocker - Add description
+Tom's GeoIP Blocker - A simple script to block certain countries from joining your Garry's Mod server.
 Created by Tom.bat (STEAM_0:0:127595314)
 Website: https://tomdotbat.dev
 Email: tom@tomdotbat.dev
