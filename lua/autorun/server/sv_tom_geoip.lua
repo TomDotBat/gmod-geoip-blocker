026b8cc7d14a92f01fbb5d5a30ccc649fea889c8
Tom <tomtheawesomest@gmail.com>
2020-05-14 00:23:57 +0100

    Add config

--- lua/autorun/server/sv_tom_geoip.lua
+++ lua/autorun/server/sv_tom_geoip.lua
@@ -7,3 +7,20 @@ Email: tom@tomdotbat.dev
 Discord: Tom.bat#0001
 ---------------------------------------------------------------------------]]
 
+local config = {}
+
+--Configuration Start--
+
+config.punishByBan = false --Should we ban the user or just kick them?
+
+config.isWhitelist = false --Should the country code list be a whitelist or blacklist?
+
+config.countryList = { --Which countries should we white/blacklist? List of country codes, use: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+    ["GB"] = true
+}
+
+config.bypassIDs = { --Who can bypass the GeoIP checks? List of SteamID64s.
+    ["13234324"] = true
+}
+
+--Configuration End--
